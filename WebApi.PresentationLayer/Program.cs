ec17f88a23d1bb2818bc9fcf9ff0dc62e67bf943
Jakub Tokarczyk <kubat260@gmail.com>
2024-05-22 11:37:19 +0200

    feat: add carter, configure and add user endpoints prototype

--- WebApi.PresentationLayer/Program.cs
+++ WebApi.PresentationLayer/Program.cs
@@ -1,12 +1,19 @@
+using Carter;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCarter();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+app.MapCarter();
 app.Run();
