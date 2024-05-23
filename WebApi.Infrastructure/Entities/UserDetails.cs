f2bbcbb142ece629fb36817db168aac61275ca44
SarcasticMoose <kubat260@gmail.com>
2024-05-24 01:17:00 +0200

    add files

---
+++ WebApi.Infrastructure/Entities/UserDetails.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApi.Infrastructure.Entities;
+
+public class UserDetails
+{
+    public int Id { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime LastActive { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string Skills { get; set; } = string.Empty;
+    public string Interests { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+    
+    //Relations
+    public int GenderId { get; set; }
+    public Gender? Gender { get; set; } = new();
+    public int UserId { get; set; } 
+    public User? User { get; set; }
+}
